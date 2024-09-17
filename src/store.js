7128c69a55ac5ecf3b1203b11f1cbd6c969c386b
Amit Gavali <79439692+aintyourcupoftea@users.noreply.github.com>
2024-09-17 16:51:59 +0530

    Revert "Working Frontend"

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import projectsReducer from './reducers/ProjectsSlice';
-
-export const store = configureStore({
-  reducer: {
-    projects: projectsReducer,
-    // Add other reducers here
-  },
-});
